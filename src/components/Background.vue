9aaa0f1c030d65559e3f1ee3cef2fb3a3110c142
misbahabroruddin <misbahabroruddin@gmail.com>
2023-10-26 21:04:43 +0700

    feat: add background component

---
+++ src/components/Background.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    v-if="product?.category === `men's clothing`"
+    :class="product?.category === `men's clothing` && `bg-men`"
+  ></div>
+  <div
+    v-else-if="product?.category === `women's clothing`"
+    :class="product?.category === `women's clothing` && `bg-women`"
+  ></div>
+  <div
+    v-else
+    :class="
+      (product?.category !== `women's clothing` ||
+        product?.category !== `men's clothing`) &&
+      `bg-unavailable`
+    "
+  ></div>
+
+  <div></div>
+</template>
+
+<script setup>
+defineProps({
+  product: Object,
+});
+</script>
