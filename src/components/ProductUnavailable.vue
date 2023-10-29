113fd75acfb19beb6665318238dbae37d815661d
misbahabroruddin <misbahabroruddin@gmail.com>
2023-10-29 07:37:56 +0700

    feat: add product unavailable

---
+++ src/components/ProductUnavailable.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-sad"></div>
+  <div class="wrapper-unavailable">
+    <p>This product is unavailable to show</p>
+    <button
+      @click="handleClick"
+      :class="
+        (product?.category !== `women's clothing` ||
+          product?.category !== `men's clothing`) &&
+        `btn-unavailable`
+      "
+    >
+      Next Product
+    </button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  product: Object,
+});
+
+const emit = defineEmits(['click']);
+
+const handleClick = () => {
+  emit('click');
+};
+</script>
